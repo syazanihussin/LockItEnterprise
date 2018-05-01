feac0e1109632ee9bf9b4438de66fd0140d4e570
syazanihussin <syazanihussin@gmail.com>
2018-05-01 14:29:43 +0800

    syazani

--- LockItWAR/src/com/lockit/controller/loginController.java
+++ LockItWAR/src/com/lockit/controller/loginController.java
@@ -2,7 +2,6 @@ package com.lockit.controller;
 
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.lockit.ejb.HouseOwnerBeanRemote;
 
 
